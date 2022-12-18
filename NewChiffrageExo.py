4f59fd2f111682af23107126c3c26a34f1494892
CORP\cobcouli <cheikhomarben.coulibaly@sogeti.com>
2022-12-18 21:58:24 +0100

    mise a jour du code

--- NewChiffrageExo.py
+++ NewChiffrageExo.py
@@ -1,24 +1,36 @@
+""" ce code à pour but de faire les stats sur l'excell avec une nouvelle approche
+  - donc la première approche est d'écrire directement les résultats dans l'excell
+  et ne pas conservé dans une varibale """
+
 import openpyxl as op
 from chiffrage_file_with_xlrd import getcolumn
 
 path = r"C:\Users\cobcouli\OneDrive - Capgemini\Documents\Python_ressources\Feuille de calcul dans Exercice python_Jouer_avec_des_donnees.xlsx"
 
+book = op.load_workbook(path)
+sheet = book['Gestion_des_Rapporteurs']
+print(sheet)
+"""delete the empty rows """
 
-def run():
-    book  = op.load_workbook(path)
-    sheet = book['Gestion_des_Rapporteurs']
-    print(sheet)
+"""1) for each scenario get the number of case it have"""
+tabtuple=[]
+tabscenarioUnique=[]
+for row in sheet:
+   if (row[2].value,row[3].value) not in tabtuple :
+      temptuple = (row[2].value,row[3].value)
+      tabtuple.append(temptuple)
+   if row[2].value not in tabscenarioUnique :
+      tabscenarioUnique.append(row[2].value)
 
-def getNumberOfCase(file,NameColum):
-    #get the colum of lettre or position
-    pass
+for tuple in tabtuple :
+   if tuple[0]:
+      pass
 
-def getColumPosition(column,sheet):
-    row = sheet[1]
-    for cell in row:
-        if cell == column:
-            return cell.column
+print(tabtuple)
 
-def nbreCasaasocieAuscenario(sheet,colunmScenario,columnCasdeTest):
-    pass
+
+def countTupleIndex1(tab1tuple):
+   for item in tab1tuple:
+      pass
+
 
--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     #chiffrage_xlrd()
-    run()
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
